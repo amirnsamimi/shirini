afb592a2a17fcf98dce8032bb12f3d27c259919d
amirnsamimi <amirnsamimi@gmail.com>
2024-03-11 11:18:37 +0330

    update on packages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
-import { Button, ButtonBase, InputBase, TextField, createStyles } from "@mui/material";
 
 function App() {
 
-  const theme = createStyles({
-    nav:{
-      color:"pink"
 
-    }
-
-
-  })
 
   return (
     <>
