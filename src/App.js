3cf6bea89eb095551ff152d122f76c66e0e24e21
amirnsamimi <amirnsamimi@gmail.com>
2024-03-11 11:15:53 +0330

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import { Button, ButtonBase, InputBase, TextField, createStyles } from "@mui/material";
 
 function App() {
+
+  const theme = createStyles({
+    nav:{
+      color:"pink"
+
+    }
+
+
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    </>
   );
 }
 
