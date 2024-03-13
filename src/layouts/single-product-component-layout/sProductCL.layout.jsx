bda095aec16d4314cdc411a4a398a6366de0eab0
amirnsamimi <amirnsamimi@gmail.com>
2024-03-13 09:32:07 +0330

    update on single-product component

--- src/layouts/single-product-component-layout/sProductCL.layout.jsx
+++ src/layouts/single-product-component-layout/sProductCL.layout.jsx
@@ -7,16 +7,18 @@ import { Box } from "@mui/material";
 const SProductCL = () => {
 
     return <Grid2 xs={12} container sx={{height:"80dvh"}}>
-    <Grid2 xs={3} container sx={{transform:"rotate(-90deg) translate(0,0)", heigth:"100%"}}>
-asdsadsadbaskjdsaj
+    <Grid2 xs={3} container sx={{writingMode:"vertical-rl", transform:"rotate(180deg)",alignItems:"flex-end", fontSize:"4rem"}}>
+
+    Explore Our 
+    Nostalgy Sweets.
    
     </Grid2>
-    <Grid2 xs={6} container>
+    <Grid2 xs={6} container  className="relative">
         <Grid2 xs={12} container justifyContent="center" alignItems={"center"} className="bg-yellow-500" >
             <img src="/assets/images/cookie.svg" width={"75%"} />
         </Grid2>
     </Grid2>
-    <Grid2 xs={3} container alignItems="center" >
+    <Grid2 xs={3} container alignItems="center" className={"relative right-20" } >
         <Grid2 xs={12}  className="bg-white" sx={{border:`3px solid black`, height:"75%",boxShadow:"10px 10px 5px black"}}>
 
         </Grid2>
